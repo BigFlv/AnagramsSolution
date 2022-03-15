a8a640b65c6254c9f50c601bdf94cc02ef83c8b9
evelina <evelina.iordache@student.tuiasi.ro>
2022-03-15 19:16:23 +0200

    modificare

--- src/ClasaLuMiron.java
+++ src/ClasaLuMiron.java
@@ -2,8 +2,15 @@
 public class ClasaLuMiron {
 	String s="Cine citeste asta va avea o zi buna!";
 	
+	
 	void Afisare()
 	{
-		System.out.println(s);
+		System.out.println("Afisati datele:");
+	}
+	
+	ClasaLuMiron()
+	{
+		System.out.println("Constructor");
 	}
 }
+
