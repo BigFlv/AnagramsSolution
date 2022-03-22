596dd1c6aa34e1777be016e0b9b855bc2831fb5a
BigFlv <vacarasu.flavia@gmail.com>
2022-03-22 14:33:01 +0200

    I generated the documentation with Javadoc adding the appropriate comments, and I did test on methods with the help of JUnit 5.

--- src/AnagramsSolution.java
+++ src/AnagramsSolution.java
@@ -1,7 +1,17 @@
 import java.util.Hashtable;
 import java.util.Scanner;
 
+/**
+ * This class has just two methods.
+ */
 public class AnagramsSolution {
+    /**
+     * This method checks if two string is Anagram. An Anagram is a word or phrase formed by rearranging the letters.
+     * We choose to make that with HashTable.
+     * @param a is first string
+     * @param b is second string
+     * @return true if a and b are Anagrams
+     */
     static boolean isAnagram(String a, String b) {
         Hashtable <Character, Integer> hm1 = new Hashtable<Character, Integer>();
         for (int i = 0; i < a.length(); i++) {
@@ -24,6 +34,12 @@ public class AnagramsSolution {
         //am adaugat ceva nou
         return hm1.equals(hm2);// I change "==" with equals
     }
+
+    /**
+     * This method returns the first number which is repetitive.
+     * @param arr is a array
+     * @return return an integer number which is repetitive
+     */
     static int findD (int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
@@ -34,6 +50,11 @@ public class AnagramsSolution {
         }
         return 0;
     }
+
+    /**
+     * Here we create instance or object of class AnagramsSolution
+     * @param args ceva
+     */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String a = scan.next();
@@ -43,11 +64,9 @@ public class AnagramsSolution {
         System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
         System.out.println("test");
 
-
         System.out.println("Commit - Opincariu Calin");
         
         // rezolvati inca doua probleme
-        
         ClasaLuMiron m=new ClasaLuMiron();
         m.Afisare();
 
--- src/ClasaLuMiron.java
+++ src/ClasaLuMiron.java
@@ -1,8 +1,8 @@
-
+/**
+ * Fara cuvinte!
+ */
 public class ClasaLuMiron {
 	String s="Cine citeste asta va avea o zi buna!";
-	
-	
 	void Afisare()
 	{
 		System.out.println("Afisati datele:");
