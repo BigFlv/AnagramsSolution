9868c6b48982aac0952927e7331f28ca55adb4cf
evelinaiordache <evelina.iordache@student.tuiasi.ro>
2022-05-17 19:50:09 +0300

    Test JUnit

---
+++ src/bridge/Test.java
@@ -0,0 +1,12 @@
+package bridge;
+
+import junit.framework.TestSuite;
+import objects.Chair;
+
+public class Test {
+	public static void suite() {
+		TestSuite suite=new TestSuite();
+	//	suite.addTest(Chair.suite());
+	}
+
+}
--- src/objects/Chair.java
+++ src/objects/Chair.java
@@ -1,4 +1,8 @@
 package objects;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
 /**
  * super apeleaza constructorul din clasa de baza
  * @author EvEe
@@ -22,6 +26,28 @@ public class Chair extends Furniture {
 		super.manufacture();
 		
 		System.out.println("----------------------------");
-
 	}
+		/**
+		 * Testare daca doua scaune au aceeasi dimensiune
+		 * 
+		 */
+		public boolean equals(Object obj) {
+			if(obj instanceof Chair) {
+				Chair c=(Chair) obj;
+				return c.getTitle().equals(getTitle());
+			}
+			return false;
+		}
+		public  Object getTitle() {
+		
+			return getTitle();
+		}
+		public static Test suite() {
+			TestSuite suite=new TestSuite();
+			suite.addTest(Chair.suite());
+			return suite;
+		}
+		
+
+	
 }
